896b23448ce545d4021a38d583af9d33f9b31e0c
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-09-19 13:04:34 +0200

    added exhange

---
+++ exchange.py
@@ -0,0 +1,6 @@
+US_to_GBP = 0.77
+dollars = 1000
+
+print(f"Today, $ {dollars}")
+print(f"The exchange GBP exchange rate {US_to_GBP:.2f}")
+print(f"{dollars} $ is equal to {dollars * US_to_GBP} pounds")
