47a14ae1fff1e826dcaa7d868df2051524b344aa
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-09-19 15:48:01 +0200

    opg 59

---
+++ byteConverter.py
@@ -0,0 +1,7 @@
+bytes = int(input())
+kilobytes = bytes/1000
+megabytes = kilobytes/1000
+gigabytes = megabytes/1000
+terabytes = gigabytes/1000
+
+print(f" {bytes} bytes = {kilobytes} kilobytes = {megabytes} megabytes = {gigabytes} gigabytes = {terabytes} terabytes")
